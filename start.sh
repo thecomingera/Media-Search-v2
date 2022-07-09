8929095893fcfa2622443923f175498fb7557cbc
RituRajPS <107362757+thecomingera@users.noreply.github.com>
2022-07-09 21:47:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b main https://github.com/Anonymous-18110/mrproviderx.git /mrproviderx
-cd /mrproviderx
+git clone -b main https://github.com/thecomingera/Media-Search-v2.git /Media-Search-v2
+cd /Media-Search-v2
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
